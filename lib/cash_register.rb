8e93b9c4cd59c549e02c64a8ffc6bdd159e9266e
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-07-29 17:25:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
 class CashRegister
   attr_accessor :employee_discount, :total
   
-  def initialize(total = 0, employee_discount = 0)
-    @total = total
+  def initialize(total, employee_discount = 0)
+    @total = 0 
+    @employee_discount = employee_discount
   end
 end
